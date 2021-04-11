9cde4ba6db3d8e4782aeab704ae0f96e6b52b73f
raknas999 <32408560+raknas999@users.noreply.github.com>
2021-04-11 23:15:53 +0530

    Update SonarScan.groovy

--- vars/SonarScan.groovy
+++ vars/SonarScan.groovy
@@ -5,7 +5,7 @@ def call() {
     }
     steps {
         withSonarQubeEnv('sonarqube') {
-            sh "${scannerHome}/bin/sonar-scanner"
+            sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=HelloWorldServlet-Pipeline -Dsonar.projectName=HelloWorldServlet-Pipeline -Dsonar.java.binaries=/var/lib/jenkins/workspace/sample-pipeline/target/classes -Dsonar.projectKey=HelloWorldServlet-Pipeline -Dsonar.sources=/var/lib/jenkins/workspace/sample-pipeline"
         }
 	}
 }
