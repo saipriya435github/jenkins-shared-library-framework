7d86ca8716aecb6c0990c1dbb12e758e08f13351
raknas999 <32408560+raknas999@users.noreply.github.com>
2021-04-11 23:05:02 +0530

    sonarscanner

---
+++ vars/SonarScan.groovy
@@ -0,0 +1,11 @@
+def call() {  
+  
+     environment {
+        scannerHome = tool 'sonarqube'
+    }
+    steps {
+        withSonarQubeEnv('sonarqube') {
+            sh "${scannerHome}/bin/sonar-scanner"
+        }
+	}
+}
